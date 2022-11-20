4c5b1c67a8ef7cda9b544d6d602842bb70fe5a23
KatiaRassadina <114443286+KatiaRassadina@users.noreply.github.com>
2022-11-20 12:58:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] array = new Double[3, 4];
+randomFilling(array);
+arrayOutput(array);
+void randomFilling(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round((new Random().NextDouble() * 100), 1);
+        }
+    }
+}
+void arrayOutput(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "  ");
+        }
+        Console.WriteLine(" ");
+    }
+}
